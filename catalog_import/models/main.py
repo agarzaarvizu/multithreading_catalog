286fa2b4939def8fef6a596375204ab7ced1166c
agarzaarvizu <hectorgarza.dev@gmail.com>
2023-06-01 17:35:59 -0600

    [ADD] catalog_import: Dynamodb connection added

--- catalog_import/models/main.py
+++ catalog_import/models/main.py
@@ -1,12 +1,14 @@
 from .connection import Connection
 from .provider import Provider
 from .products import Products
+from boto3.dynamodb.conditions import Attr
 
 
 class Main:
     def __init__(self, **kwargs) -> None:
         self.provider_name = kwargs.get("provider_name")
         self.provider_values = kwargs.get("provider_values")
+        self.table = kwargs.get("table")
 
     def create_provider(self):
         """
@@ -62,6 +64,15 @@ class Main:
         """
         return Products(**kwargs)
 
+    def check_product_dynamodb(self, **kwargs):
+        response = self.table.scan(FilterExpression=Attr("MPN").contains(kwargs.get("mpn_value")))
+        count = response.get("Count")
+        if count == 0:
+            response = self.table.put_item(Item=kwargs.get("product"))
+            print(response)
+        else:
+            print("Product already in the DB")
+
     def execute(self):
         products = []
         response = self.get_provider_response()
@@ -70,4 +81,7 @@ class Main:
             products = product.parse_response_icecat()
         elif self.provider_name.startswith("Etilize"):
             products = product.parse_response_etilize()
-        print(len(products))
+        
+        product = products[0]
+        mpn_value = product.get("MPN")
+        self.check_product_dynamodb(mpn_value=mpn_value, product=product)
--- execute.py
+++ execute.py
@@ -1,19 +1,25 @@
 from catalog_import.data import providers
 from catalog_import.models import main as main_model
 from threading import Thread
+import boto3
+import time
 
 
 def main(**kwargs):
+    time.sleep(1)
     main = main_model.Main(**kwargs)
     main.execute()
 
 def execute(event, context):
     threads = []
     providers_dict = providers.providers
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('test_multithread_db')
     for provider_name, provider_values in providers_dict.items():
         provider = {
             "provider_name": provider_name,
             "provider_values": provider_values,
+            "table": table,
         }
         x = Thread(target=main, kwargs=provider)
         threads.append(x)
