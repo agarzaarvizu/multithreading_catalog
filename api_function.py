7aace6c50960a23e54b7db0241428c939c97a2c4
agarzaarvizu <hectorgarza.dev@gmail.com>
2023-06-21 17:08:11 -0600

    [ADD] api_function: Api key verification and categories added

--- api_function.py
+++ api_function.py
@@ -23,7 +23,16 @@ def check_languages(parameters):
     languages = [value for parameter, value in parameters.items() if parameter.lower()=="language"]
     return get_unique_values(languages)
 
-def filter_data_language(language, product_list):
+def check_categories(client_name, category_table, categories):
+    client_categories = []
+    for category in categories:
+        category_name = category.get("Name")
+        found_category = category_table.scan(FilterExpression=Attr('CategoryName').eq(category_name) & Attr('ClientName').eq(client_name))['Items']
+        if found_category:
+            client_categories += found_category
+    return client_categories
+
+def filter_data_language(client_name, language, product_list, category_table):
     result = []
     for product in product_list:
         attributes = []
@@ -49,9 +58,10 @@ def filter_data_language(language, product_list):
                 }
                 attributes.append(attribute_value)
 
+        client_categories = check_categories(client_name, category_table, categories)
         filtered_product.update(
             {
-                "Categories": categories,
+                "Categories": client_categories,
                 "Descriptions": descriptions,
                 "Gallery": gallery,
                 "Attributes": attributes
@@ -60,7 +70,7 @@ def filter_data_language(language, product_list):
         result.append(filtered_product)
     return result
 
-def get_filtered_products(table, parameters):
+def get_filtered_products(client_name, products_table, category_table, parameters):
     identifiers_list = check_identifiers_keys(parameters)
     languages_list = check_languages(parameters)
     if len(identifiers_list) >= 2:
@@ -72,23 +82,42 @@ def get_filtered_products(table, parameters):
     parameters.pop("language", None)
     for parameter, value in parameters.items():
         if parameter in IDENTIFIERS:
-            response = table.scan(FilterExpression=Attr(parameter).contains(value))
+            response = products_table.scan(FilterExpression=Attr(parameter).contains(value))
             product_list = response["Items"]
-            data = filter_data_language(language, product_list)
+            data = filter_data_language(client_name, language, product_list, category_table)
             break
         else:
             data = "Invalid Paremeter: %s" % parameter
     return data
 
+def get_client(client_key, api_keys_register):
+    client = api_keys_register.scan(FilterExpression=Attr('api_key').eq(client_key))['Items']
+    if client:
+        client_name = client[0].get('client_name')
+    else:
+        client_name = False
+    return client_name
+
 def lambda_handler(event, context):
     data = "Not Found"
     try:
+        client_key = event.get('context', {}).get('api-key')
         dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table('test_multithread_db')
+        products_table = dynamodb.Table('test_multithread_db')
+        categories_table = dynamodb.Table('data_providers')
+        api_keys_register = dynamodb.Table('ClientAPIKeys')
+        client_name = get_client(client_key, api_keys_register)
+        
+        if not client_name:
+            return {
+                'statusCode': 400,
+                'body': json.dumps('This client is not registered')
+            }
+        
         params = event.get("params", False)
         query_params = params.get("querystring")
         if query_params:
-            data = get_filtered_products(table, query_params)
+            data = get_filtered_products(client_name, products_table, categories_table, query_params)
         else:
             data = "You must specify the product using the MPN, UPC or EAN"
         return {
